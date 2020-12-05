1252ea86f1b4cd7a9906181df6232ea7002d069b
Nick keers <nickkeers@gmail.com>
2020-12-05 18:24:14 +0000

    Reformat code

--- src/main/scala/days/Day2.scala
+++ src/main/scala/days/Day2.scala
@@ -1,10 +1,11 @@
 package days
 
+import scala.language.postfixOps
 import scala.util.matching.Regex
 import scala.util.parsing.combinator._
-import scala.language.postfixOps
 
 class Day2 extends Solution {
+
   class PuzzleLine(lowerBound: Int, upperBound: Int, letter: Char, word: List[Char]) {
     def letterCount(chars: List[Char]): Map[Char, Int] = {
       chars.groupBy(identity).view.mapValues(_.size).toMap
@@ -15,34 +16,40 @@ class Day2 extends Solution {
     }
 
     def success2(): Boolean = {
-      letterCount(List(lowerBound-1, upperBound-1).map(word)).getOrElse(letter, 0) == 1
+      letterCount(List(lowerBound - 1, upperBound - 1).map(word)).getOrElse(letter, 0) == 1
     }
   }
 
   object PuzzleLineParser extends RegexParsers {
     override val whiteSpace: Regex = """[ \t]+""".r
 
-    private def boundNum: Parser[Int] = """(\d+)""".r ^^ {
-      _.toInt
-    }
+    private def boundNum: Parser[Int] =
+      """(\d+)""".r ^^ {
+        _.toInt
+      }
 
-    private def char: Parser[String] = """[a-z]""".r <~ ":" ^^ {
-      letter => letter }
+    private def char: Parser[String] =
+      """[a-z]""".r <~ ":" ^^ {
+        letter => letter
+      }
 
-    private def word: Parser[String] = """[a-z]+""".r ^^ { _.toString }
+    private def word: Parser[String] =
+      """[a-z]+""".r ^^ {
+        _.toString
+      }
 
     private def bounds: Parser[(Int, Int)] = boundNum ~ "-" ~ boundNum ^^ {
       case a ~ _ ~ b =>
-        (a,b)
+        (a, b)
     }
 
     val eol: Regex = """[\r?\n]+""".r
 
     private def puzzleLine: Parser[PuzzleLine] = opt(eol) ~> bounds ~ char ~ word <~ opt(eol) ^^ {
-      case (a,b) ~ letter ~ word => new PuzzleLine(a, b, letter.charAt(0), word.toList)
+      case (a, b) ~ letter ~ word => new PuzzleLine(a, b, letter.charAt(0), word.toList)
     }
 
-    def puzzleLines: Parser[List[PuzzleLine]] =  rep1(puzzleLine)
+    def puzzleLines: Parser[List[PuzzleLine]] = rep1(puzzleLine)
 
 
     def apply(input: String): List[PuzzleLine] = {
@@ -58,11 +65,12 @@ class Day2 extends Solution {
     }
   }
 
-  val input: List[PuzzleLine] =  PuzzleLineParser(readInputRaw(2))
+  val input: List[PuzzleLine] = PuzzleLineParser(readInputRaw(2))
 
   def partA(): Int = {
     input.count(pl => pl.success())
   }
+
   def partB(): Any = {
     input.count(p => p.success2())
   }
--- src/main/scala/days/Solution.scala
+++ src/main/scala/days/Solution.scala
@@ -4,7 +4,10 @@ import days.lib.Control
 
 trait Solution {
   def readInput(day: Int): List[String] = Control.readTextFile(s"src/day${day}.txt").get
+
   def readInputRaw(day: Int): String = Control.readTextFileRaw(s"src/day${day}.txt").get
+
   def partA(): Any
+
   def partB(): Any
 }
