c0bc2d481e530b8d0dc4a4a37d9be48ffcda2c45
Nick keers <nickkeers@gmail.com>
2020-12-05 18:22:43 +0000

    Move puzzle parser into the Day2 class to tidy it up. One class per file and all

--- src/main/scala/days/Day2.scala
+++ src/main/scala/days/Day2.scala
@@ -3,63 +3,62 @@ package days
 import scala.util.matching.Regex
 import scala.util.parsing.combinator._
 import scala.language.postfixOps
-class PuzzleLine(lowerBound: Int, upperBound: Int, letter: Char, word: List[Char]) {
-  def letterCount(chars: List[Char]): Map[Char, Int] = {
-    chars.groupBy(identity).view.mapValues(_.size).toMap
-  }
-
-  def success(): Boolean = {
-    (lowerBound to upperBound) contains letterCount(word).getOrElse(letter, 0)
-  }
-
-  def success2(): Boolean = {
-    letterCount(List(lowerBound-1, upperBound-1).map(word)).getOrElse(letter, 0) == 1
-  }
-}
-
-object PuzzleLineParser extends RegexParsers {
-  override val whiteSpace: Regex = """[ \t]+""".r
-
-  private def boundNum: Parser[Int] = """(\d+)""".r ^^ {
-    _.toInt
-  }
-
-  private def char: Parser[String] = """[a-z]""".r <~ ":" ^^ {
-    letter => letter }
-
-  private def word: Parser[String] = """[a-z]+""".r ^^ { _.toString }
-
-  private def bounds: Parser[(Int, Int)] = boundNum ~ "-" ~ boundNum ^^ {
-    case a ~ _ ~ b =>
-      (a,b)
-  }
-
-  val eol: Regex = """[\r?\n]+""".r
-
-  private def puzzleLine: Parser[PuzzleLine] = opt(eol) ~> bounds ~ char ~ word <~ opt(eol) ^^ {
-    case (a,b) ~ letter ~ word => new PuzzleLine(a, b, letter.charAt(0), word.toList)
-  }
-
-  def puzzleLines: Parser[List[PuzzleLine]] =  rep1(puzzleLine)
-
-
-  def apply(input: String): List[PuzzleLine] = {
-    parseAll(puzzleLines, input) match {
-      case Success(x, _) => x
-      case Error(x, _) =>
-        println(s"ERROR: ${x}")
-        List()
-      case Failure(x, in) =>
-        println(s"FAILURE: ${x}")
-        List()
-    }
-  }
-}
-
 
 class Day2 extends Solution {
-  val input: List[PuzzleLine] =  PuzzleLineParser(readInputRaw(2))
+  class PuzzleLine(lowerBound: Int, upperBound: Int, letter: Char, word: List[Char]) {
+    def letterCount(chars: List[Char]): Map[Char, Int] = {
+      chars.groupBy(identity).view.mapValues(_.size).toMap
+    }
 
+    def success(): Boolean = {
+      (lowerBound to upperBound) contains letterCount(word).getOrElse(letter, 0)
+    }
+
+    def success2(): Boolean = {
+      letterCount(List(lowerBound-1, upperBound-1).map(word)).getOrElse(letter, 0) == 1
+    }
+  }
+
+  object PuzzleLineParser extends RegexParsers {
+    override val whiteSpace: Regex = """[ \t]+""".r
+
+    private def boundNum: Parser[Int] = """(\d+)""".r ^^ {
+      _.toInt
+    }
+
+    private def char: Parser[String] = """[a-z]""".r <~ ":" ^^ {
+      letter => letter }
+
+    private def word: Parser[String] = """[a-z]+""".r ^^ { _.toString }
+
+    private def bounds: Parser[(Int, Int)] = boundNum ~ "-" ~ boundNum ^^ {
+      case a ~ _ ~ b =>
+        (a,b)
+    }
+
+    val eol: Regex = """[\r?\n]+""".r
+
+    private def puzzleLine: Parser[PuzzleLine] = opt(eol) ~> bounds ~ char ~ word <~ opt(eol) ^^ {
+      case (a,b) ~ letter ~ word => new PuzzleLine(a, b, letter.charAt(0), word.toList)
+    }
+
+    def puzzleLines: Parser[List[PuzzleLine]] =  rep1(puzzleLine)
+
+
+    def apply(input: String): List[PuzzleLine] = {
+      parseAll(puzzleLines, input) match {
+        case Success(x, _) => x
+        case Error(x, _) =>
+          println(s"ERROR: ${x}")
+          List()
+        case Failure(x, in) =>
+          println(s"FAILURE: ${x}")
+          List()
+      }
+    }
+  }
+
+  val input: List[PuzzleLine] =  PuzzleLineParser(readInputRaw(2))
 
   def partA(): Int = {
     input.count(pl => pl.success())
